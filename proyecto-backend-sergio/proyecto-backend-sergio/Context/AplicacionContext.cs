cd7a160daede1d9334402663fefa7e43928c137a
Sergio <sucapucasergio2.com>
2023-12-04 14:31:11 -0400

    subiendo del modelado

---
+++ proyecto-backend-sergio/proyecto-backend-sergio/Context/AplicacionContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Examen.Models;
+
+
+namespace Examen.Context
+{
+    public class AplicacionContext: DbContext
+    {
+        public AplicacionContext(DbContextOptions<AplicacionContext> options)
+       : base(options) { }
+        public DbSet<Avion> Avion { get; set; }
+        public DbSet<Hangares> Hangares { get; set; }
+        public DbSet<Piloto> Piloto { get; set; }
+
+
+    }
+}
